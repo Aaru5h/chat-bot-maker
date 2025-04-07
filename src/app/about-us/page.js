45ff5768b9cef1dd8de2f8bf7c1bc5d31fe6c619
Aaru5h <mestrikerocks@gmail.com>
2025-04-07 20:25:55 +0530

    added routes

---
+++ src/app/about-us/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>AboutUs: We develop, we rock!!!</div>
+  )
+}
+
+export default AboutUs
---
+++ src/app/auth/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>Authorisation Page</div>
+  )
+}
+
+export default Auth
