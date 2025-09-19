ca1aaf678f37f745a4679103f726f1423080bad2
Aaru5h <mestrikerocks@gmail.com>
2025-09-19 18:13:18 +0530

    error fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isDev = process.env.NODE_ENV === "development";
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' ${isDev ? "'unsafe-eval'" : ""};
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              connect-src 'self' https:;
+              font-src 'self';
+            `.replace(/\n/g, ""), // remove newlines
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
