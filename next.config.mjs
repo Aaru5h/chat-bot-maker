c312ff80af05ea95113aeae15c740d8c6aeb378a
Aaru5h <mestrikerocks@gmail.com>
2025-09-19 18:15:26 +0530

    checkpoint

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV === "development";
-
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' ${isDev ? "'unsafe-eval'" : ""};
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data:;
-              connect-src 'self' https:;
-              font-src 'self';
-            `.replace(/\n/g, ""), // remove newlines
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
